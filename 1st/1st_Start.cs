1f83b1e524cc43cfa6cf50b7d302b23675845dcf
Xiaoqi_Weng <50273091+zjnbwxq@users.noreply.github.com>
2023-10-18 22:29:06 +0100

    Add files via upload

---
+++ 1st/1st_Start.cs
@@ -0,0 +1,18 @@
+using System; //引入命名空间
+
+namespace _001_Start //定义命名空间
+{
+    class Program //类
+    {
+        static void Main(string[] args) //方法
+        {
+            Console.WriteLine("Hello World!"); //输出Hello World
+        }
+    }
+}
+
+// What is comment?
+// There have 2 kinds of comment.
+// 1.single line comment(use "//"to comment single line, but the 2nd line will be ignored)
+// 2.multi line comment(use "/*" to comment multi line, and "*/" to end comment)
+// quick key to comment: ctrl + k, ctrl + c / uncomment: ctrl + k, ctrl + u
